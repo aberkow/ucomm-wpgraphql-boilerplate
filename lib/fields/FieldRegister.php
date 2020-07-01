f8c97d67d62c93c3f2fa87efe85435e63cdc5b8b
Adam Berkowitz <adam.berkowitz@uconn.edu>
2020-07-01 11:00:33 -0400

    breaking out methods and a little documentation

--- lib/fields/FieldRegister.php
+++ lib/fields/FieldRegister.php
@@ -8,34 +8,39 @@ class FieldRegister {
 
   }
 
-  public function registerMyField() {
-    (new MyField('RootQuery', 'myField'))->registerField();
-  }
-
-  public function setFields(array $myFields = [], string $prefix = ''): array {
+  public function setFields(array $fieldsConfig = []): array {
     // if there is no field config array, get it from the data directory
-    if (count($myFields) === 0) {
-      $myFields = include BGQLE_DIR . '/lib/data/fields.php';
+    if (count($fieldsConfig) === 0) {
+      $fieldsConfig = include BGQLE_DIR . '/lib/data/fields.php';
     }
 
+    // right now this will only work for a flat array of namespaces. it might need to be made recursive somehow.
     return array_map(function($namespace, $configurations) {
-
+      $registeredClasses = [];
       foreach ($configurations as $config) {
-        // create a class name for every namespace and field
-        $class = $namespace . $config['className'];
-        
-        // resolve a new class from from the class name
-        // use the connection and fieldName properties as constructor arguments
-        $resolved = new $class($config['connection'], $config['fieldName']);
-
-        // register the field type using the resolved class in a closure
-        add_action('graphql_register_types', function() use ($resolved) {
-          $resolved->registerField();
-        });
+        $registered = $this->createRegistry($namespace, $config);
+        $registeredClasses[] = $registered;
       }
 
       // return the resolved class to keep track of it.
-      return $resolved;
-    }, array_keys($myFields), $myFields);
+      return $registeredClasses;
+    }, array_keys($fieldsConfig), $fieldsConfig);
+  }
+
+  public function createRegistry($namespace, $config)
+  {
+    // create a class name for every namespace and field
+    $class = $namespace . $config['className'];
+
+    // resolve a new class from from the class name
+    // use the connection and fieldName properties as constructor arguments
+    $resolved = new $class($config['connection'], $config['fieldName']);
+
+    // register the field type using the resolved class in a closure
+    add_action('graphql_register_types', function () use ($resolved) {
+      $resolved->registerField();
+    });
+
+    return $resolved;
   }
 }
